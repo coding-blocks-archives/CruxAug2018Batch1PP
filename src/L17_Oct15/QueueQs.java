9259111aef711882766cfaae3568c4cb4bf704ab
Garima Chhikara <garimachhikara128@gmail.com>
2018-10-16 22:07:52 +0530

    Stack, Queue Q's

---
+++ src/L17_Oct15/QueueQs.java
@@ -0,0 +1,62 @@
+package L17_Oct15;
+
+import L15_Oct7.DynamicQueue;
+import L15_Oct7.Queue;
+
+/**
+ * @author Garima Chhikara
+ * @email garima.chhikara@codingblocks.com
+ * @date 15-Oct-2018
+ *
+ */
+
+public class QueueQs {
+
+	public static void main(String[] args) throws Exception {
+
+		Queue q = new DynamicQueue();
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		q.enqueue(50);
+
+		q.display();
+		displayReverse(q, 0);
+		q.display();
+
+		actualReverse(q);
+		q.display();
+
+	}
+
+	public static void displayReverse(Queue q, int count) throws Exception {
+
+		if (count == q.size()) {
+			return;
+		}
+
+		int temp = q.dequeue();
+		q.enqueue(temp);
+
+		displayReverse(q, count + 1);
+
+		System.out.println(temp);
+
+	}
+
+	public static void actualReverse(Queue q) throws Exception {
+
+		if (q.isEmpty()) {
+			return;
+		}
+
+		int temp = q.dequeue();
+
+		actualReverse(q);
+
+		q.enqueue(temp);
+
+	}
+
+}
---
+++ src/L17_Oct15/StackQs.java
@@ -0,0 +1,137 @@
+package L17_Oct15;
+
+import L14_Oct6.Stack;
+import L15_Oct7.DynamicStack;
+
+/**
+ * @author Garima Chhikara
+ * @email garima.chhikara@codingblocks.com
+ * @date 15-Oct-2018
+ *
+ */
+
+public class StackQs {
+
+	public static void main(String[] args) throws Exception {
+
+		Stack s = new DynamicStack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		s.push(40);
+		s.push(50);
+
+		s.display();
+		displayReverse(s);
+		s.display();
+
+		actualReverse(s, new DynamicStack());
+
+		s.display();
+
+		int[] arr = { 10, 20, 3, 2, 5, 1, 70, 60 };
+		nextGreaterElement(arr);
+
+		int[] price = { 15, 20, 17, 30, 25, 13, 27, 14, 20, 35 };
+
+		int[] span = stockSpan(price);
+		for (int val : span) {
+			System.out.print(val + "  ");
+		}
+		System.out.println();
+	}
+
+	public static void displayReverse(Stack s) throws Exception {
+
+		if (s.isEmpty()) {
+			return;
+		}
+
+		int temp = s.pop();
+		displayReverse(s);
+
+		System.out.println(temp);
+
+		s.push(temp);
+
+	}
+
+	public static void actualReverse(Stack s, Stack helper) throws Exception {
+
+		if (s.isEmpty()) {
+			actualReverseHelper(s, helper);
+
+			return;
+		}
+
+		int temp = s.pop();
+		helper.push(temp);
+
+		actualReverse(s, helper);
+
+	}
+
+	private static void actualReverseHelper(Stack s, Stack helper) throws Exception {
+
+		if (helper.isEmpty()) {
+			return;
+		}
+
+		int temp = helper.pop();
+		actualReverseHelper(s, helper);
+		s.push(temp);
+
+	}
+
+	public static void nextGreaterElement(int[] arr) throws Exception {
+
+		Stack s = new DynamicStack();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			while (!s.isEmpty() && s.peek() < arr[i]) {
+
+				int temp = s.pop();
+				System.out.println(temp + " -> " + arr[i]);
+
+			}
+
+			s.push(arr[i]);
+
+		}
+
+		while (!s.isEmpty()) {
+
+			int temp = s.pop();
+			System.out.println(temp + "-> -1");
+
+		}
+
+	}
+
+	public static int[] stockSpan(int[] prices) throws Exception {
+
+		int[] span = new int[prices.length];
+
+		Stack s = new DynamicStack();
+
+		for (int i = 0; i < prices.length; i++) {
+
+			while (!s.isEmpty() && prices[s.peek()] < prices[i]) {
+				s.pop();
+			}
+
+			if (s.isEmpty()) {
+				span[i] = i + 1;
+			} else {
+				span[i] = i - s.peek();
+			}
+
+			s.push(i);
+		}
+
+		return span;
+
+	}
+
+}
